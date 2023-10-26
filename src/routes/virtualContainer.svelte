36d652d3b9845d9cd33a8839da9553508b0e6880
pgleamy <pgleamy@gmail.com>
2023-10-26 17:14:09 -0600

    ...

--- src/routes/virtualContainer.svelte
+++ src/routes/virtualContainer.svelte
@@ -15,6 +15,8 @@
     } from '$lib/scrollStore.js'; 
   
     const fixedOffset = 5000000;
+    const massiveContainerHeight = '10000000px';
+    const itemLimit = 3; // Number of items to keep in the conversation[] array
     let itemCount = 0; // Number of items in the conversation[]
     let relativeOffsets = []; // Relative offsets of each item
     let itemHeights = []; // Height of each item in pixels
@@ -33,26 +35,7 @@
     let unsubscribe = conversation.subscribe(value => {
       currentConversation = value;
   
-      // Logic to update itemCount, relativeOffsets, and messageCounts
-      
-      // Your existing code for determining new messages and updating arrays
-      itemCount++;
-      const fetchedMessageCount = get('fetchedMessageCount') || 0; // Retrieve the number of fetched messages from local storage
-      messageCounts.push(fetchedMessageCount); // number of messages fetches as reported in local storage
-      const newRelativeOffset = 0/* logic to calculate new offset based on added messages */;
-      relativeOffsets.push(newRelativeOffset);
-  
-      // Logic to trim messages based on itemCount and scrollingDirection
-      if (itemCount > 3) {
-        trimArrays(scrollingDirection);
-      }
-
-      // Calculate renderedStartOffset and renderedEndOffset then output to console log
-      // a report of the current state of the conversation[] array as a table
-      renderedStartOffset = relativeOffsets[0];
-      let storedHeight = updateLastItemHeight(); 
-      let lastItemHeight = storedHeight ? storedHeight : 0;
-      renderedEndOffset = relativeOffsets[itemCount - 1] + lastItemHeight;
+      updateConversationState();
 
       // Very detailed console log that creates a table showing the values of all of the current conversation[] items labelled and stacked vertically
       let tableData = [];
@@ -70,25 +53,44 @@
     }); // End of conversation[] subscribe
 
 
+    function updateConversationState() {
+        // Step 1: Perform trimming if necessary
+        if (itemCount > itemLimit) {
+            trimArrays(scrollingDirection);
+        }
 
-    // Obtains the pixel height of the last item in the conversation[] array
-    function updateLastItemHeight() {
+        // Step 2: Update itemCount
+        itemCount++;
+
+        // Step 3: Update fetchedMessageCount and messageCounts
+        const fetchedMessageCount = get('fetchedMessageCount') || 0;
+        messageCounts.push(fetchedMessageCount);
+
+        // Step 4: Update itemHeights and lastItemHeight
         let lastItemHeight = 0;
-        try {
-            const storedHeight = get('messageHeight'); 
-            lastItemHeight = storedHeight ? storedHeight : 0;
-        } catch (error) {
-            console.log('Failed to retrieve messageHeight from local storage: ', error);
-        }
-
-        // Based on the scrolling direction, either append or prepend to itemHeights[]
         if (scrollingDirection === 'DOWN') {
-            itemHeights.push(lastItemHeight);
+            const storedHeight = get('messageHeight') || 0;
+            itemHeights.push(storedHeight);
+            lastItemHeight = storedHeight;
         } else if (scrollingDirection === 'UP') {
-            itemHeights.unshift(lastItemHeight);
-        }
-    }
+            const storedHeight = get('messageHeight') || 0;
+            itemHeights.unshift(storedHeight);
+            lastItemHeight = storedHeight;
+        } 
 
+        // Step 5: Update newRelativeOffset and relativeOffsets
+        let newRelativeOffset;
+        if (scrollingDirection === 'DOWN') {
+            newRelativeOffset = itemHeights.slice(0, -1).reduce((acc, val) => acc + val, 0);
+        } else if (scrollingDirection === 'UP') {
+            newRelativeOffset = renderedStartOffset - lastItemHeight;
+        }
+        relativeOffsets.push(newRelativeOffset);
+
+        // Step 6: Update renderedStartOffset and renderedEndOffset
+        renderedStartOffset = relativeOffsets[0];
+        renderedEndOffset = relativeOffsets[itemCount - 1] + lastItemHeight;
+    } // End of updateConversationState()
 
 
     function trimArrays(direction) {
@@ -109,11 +111,9 @@
         }
         
         itemCount--;
-    }
+    } // End of trimArrays()
 
 
-
-  
 
     onMount(() => {
       return () => {
@@ -124,8 +124,10 @@
 
 </script>
 
+
+
 <!-- Virtual Container -->
-<div id="virtual-container" style="height: 10000000px; position: relative;">
+<div id="virtual-container" style="height: { massiveContainerHeight }; position: relative;">
   <div id="conversation-container" style="position: relative; top: {fixedOffset}px;">
     <!-- The actual messages would be rendered here by ConversationContainer.svelte -->
   </div>
